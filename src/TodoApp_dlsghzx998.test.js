252cec90b59cff1380834a36145de113e141df9b
dlsghzx998 <dlsghzx998@gmail.com>
2022-10-18 11:31:38 +0900

    TodoApp TodoForm TodoList render test ok

--- src/TodoApp_dlsghzx998.test.js
+++ src/TodoApp_dlsghzx998.test.js
@@ -8,4 +8,8 @@ describe('<TodoApp_dlsghzx998 />', () => {
         getByText('등록'); // TodoForm 화면에 있는지 확인
         getByTestId('TodoList'); // TodoList 화면에 있는지 확인
     });
-});
+    it('renders two defaults todos', () => {
+        const {getByText} = render(<TodoApp_dlsghzx998/>);
+        getByText('TDD 배우기');
+        getByText('react-testing-library');
+    })
